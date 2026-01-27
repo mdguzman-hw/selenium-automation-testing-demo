86c2092c1ba97cc438cc4960a675c237e0a8f881
Mark De Guzman <mdguzman@homewoodhealth.com>
2026-01-27 14:35:24 -0700

    Update Header class to be locale aware Wire in Header tests for smoke

--- src/tests/common/Header.ts
+++ src/tests/common/Header.ts
@@ -1,28 +1,35 @@
 import { appendFile } from 'node:fs/promises';
 import { Browser, Builder, By, WebDriver } from 'selenium-webdriver';
-import { CLICK_DELAY, HEADER, HOMEWEB_LANDING_URL_EN } from '../../Constants';
-import { generateSummary, getLogFilename } from '../../utility/Utility';
+import { CLICK_DELAY, HEADER, HOMEWEB_LANDING_URL_EN, HOMEWEB_LANDING_URL_FR } from '../../Constants';
+import { generateSummary, generateLogFileName } from '../../utility/Utility';
 
 export class Header {
     /**
      * Member Variables
      */
     private chromeDriver: WebDriver;
+    private readonly locale: string;
     private readonly logFilename: string;
+    private readonly totalTests: number;
 
-    private totalTests: number = 3;
-    private startTime: number = 0;
-
-    private passCounter: number = 0;
-    private failCounter: number = 0;
-
+    private startTime: number;
+    private passCounter: number;
+    private failCounter: number;
+    private targetURL: string;
 
     /**
      * Constructor
      */
-    constructor() {
+    constructor(locale: string) {
         this.chromeDriver = new Builder().forBrowser(Browser.CHROME).build();
-        this.logFilename = getLogFilename();
+        this.locale = locale;
+        this.logFilename = generateLogFileName(this.locale);
+        this.totalTests = 3;
+
+        this.startTime = 0;
+        this.passCounter = 0;
+        this.failCounter = 0;
+        this.targetURL = ''
     }
 
     private async runStep(cssSelector: string, stepCode: string) {
@@ -39,32 +46,64 @@ export class Header {
             this.failCounter += 1;
         } finally {
             // Always reset to homepage
-            await this.chromeDriver.get(HOMEWEB_LANDING_URL_EN);
+            await this.chromeDriver.get(this.targetURL);
         }
     }
 
     // TODO: FR tests
     public async run() {
+
+        let LABEL_LOGO = '';
+        let LABEL_TOGGLE = '';
+        let LABEL_BUTTON = '';
+
+        let ID_LOGO = '';
+        let ID_TOGGLE = '';
+        let ID_BUTTON = '';
+
+        switch (this.locale) {
+            case 'en':
+                this.targetURL = HOMEWEB_LANDING_URL_EN;
+                LABEL_LOGO = HEADER.EN_LOGO;
+                LABEL_TOGGLE = HEADER.EN_TOGGLE;
+                LABEL_BUTTON = HEADER.EN_BUTTON;
+                ID_LOGO = 'HEADER-ANON-EN-001';
+                ID_TOGGLE = 'HEADER-ANON-EN-002';
+                ID_BUTTON = 'HEADER-ANON-EN-003';
+                break;
+            case 'fr':
+                this.targetURL = HOMEWEB_LANDING_URL_FR;
+                LABEL_LOGO = HEADER.FR_LOGO;
+                LABEL_TOGGLE = HEADER.FR_TOGGLE;
+                LABEL_BUTTON = HEADER.FR_BUTTON;
+                ID_LOGO = 'HEADER-ANON-FR-001';
+                ID_TOGGLE = 'HEADER-ANON-FR-002';
+                ID_BUTTON = 'HEADER-ANON-FR-003';
+                break;
+        }
+
         this.startTime = Date.now();
         try {
-            await this.chromeDriver.get(HOMEWEB_LANDING_URL_EN);
+            await this.chromeDriver.get(this.targetURL);
+            const message = `TEST::${this.targetURL}\n`
+            await appendFile(this.logFilename, message);
 
             await this.chromeDriver.sleep(CLICK_DELAY);
             await this.runStep(
-                HEADER.EN_LOGO,
-                'HEADER-ANON-001'
+                LABEL_LOGO,
+                ID_LOGO
             );
 
             await this.chromeDriver.sleep(CLICK_DELAY);
             await this.runStep(
-                HEADER.EN_TOGGLE,
-                'HEADER-ANON-002'
+                LABEL_TOGGLE,
+                ID_TOGGLE
             );
 
             await this.chromeDriver.sleep(CLICK_DELAY);
             await this.runStep(
-                HEADER.EN_BUTTON,
-                'HEADER-ANON-003'
+                LABEL_BUTTON,
+                ID_BUTTON
             );
 
             await this.chromeDriver.sleep(CLICK_DELAY);
--- src/tests/smoke.ts
+++ src/tests/smoke.ts
@@ -1,4 +1,3 @@
-import { headerAnonEn } from './common/header-anon-en';
 import { Header } from './common/Header';
 
 /**
@@ -6,9 +5,17 @@ import { Header } from './common/Header';
  */
 async function main() {
     try {
-        // await headerAnonEn();
-        const header = new Header();
-        await header.run();
+        /**
+         * 1: Header Tests
+         */
+
+        // 1.1: EN
+        const header_en = new Header('en');
+        await header_en.run();
+
+        // 1.2: FR
+        const header_fr = new Header('fr');
+        await header_fr.run();
     }
     catch (error: any) {
         console.error('MAIN->onFailure', error);
