e939265f4e1e9f3c377f2de73616ce9849958983
Mark De Guzman <mdguzman@homewoodhealth.com>
2026-01-28 11:14:46 -0700

    Add initial Footer tests

--- src/Constants.ts
+++ src/Constants.ts
@@ -30,4 +30,27 @@ export const enum HEADER {
     FR_BUTTON = LABEL_SIGN_IN_BUTTON_FR
 }
 
+/**
+ * Footer - Labels
+ */
+const LABEL_HOMEWOOD_ABOUT_EN = 'a[href="/en/about"]';
+const LABEL_HOMEWOOD_TERMS_EN = 'a[href="/en/terms-of-service"]';
+const LABEL_HOMEWOOD_PRIVACY_EN = 'a[href="/en/privacy-policy"]';
+const LABEL_HOMEWOOD_ACCESSIBILITY_EN = 'a[href="/en/accessibility"]';
+
+const LABEL_HOMEWOOD_ABOUT_FR = 'a[href="/fr/about"]';
+const LABEL_HOMEWOOD_TERMS_FR = 'a[href="/fr/terms-of-service"]';
+const LABEL_HOMEWOOD_PRIVACY_FR = 'a[href="/fr/privacy-policy"]';
+const LABEL_HOMEWOOD_ACCESSIBILITY_FR = 'a[href="/fr/accessibility"]';
+
+export const enum FOOTER {
+    EN_ABOUT = LABEL_HOMEWOOD_ABOUT_EN,
+    EN_TERMS = LABEL_HOMEWOOD_TERMS_EN,
+    EN_PRIVACY = LABEL_HOMEWOOD_PRIVACY_EN,
+    EN_ACCESSIBILITY = LABEL_HOMEWOOD_ACCESSIBILITY_EN,
+    FR_ABOUT = LABEL_HOMEWOOD_ABOUT_FR,
+    FR_TERMS = LABEL_HOMEWOOD_TERMS_FR,
+    FR_PRIVACY = LABEL_HOMEWOOD_PRIVACY_FR,
+    FR_ACCESSIBILITY = LABEL_HOMEWOOD_ACCESSIBILITY_FR,
+}
 
---
+++ src/tests/common/Footer.ts
@@ -0,0 +1,152 @@
+import { appendFile } from 'node:fs/promises';
+import { By, WebDriver } from 'selenium-webdriver';
+import { CLICK_DELAY, FOOTER } from '../../Constants';
+import { generateSummary, generateLogFileName } from '../../Utility';
+
+/**
+ * Footer
+ */
+export class Footer {
+    /**
+     * Member Variables
+     */
+    private chromeDriver: WebDriver;
+    private readonly logFilename: string;
+    public readonly totalTests: number;
+    private readonly targetURL: string;
+
+    private startTime: number;
+    public passCounter: number;
+    public failCounter: number;
+
+    private readonly LABEL_ABOUT: string;
+    private readonly LABEL_TERMS: string;
+    private readonly LABEL_PRIVACY: string;
+    private readonly LABEL_ACCESSIBILITY: string;
+    private readonly ID_ABOUT: string;
+    private readonly ID_TERMS: string;
+    private readonly ID_PRIVACY: string;
+    private readonly ID_ACCESSIBILITY: string;
+
+    /**
+     * Constructor
+     */
+    constructor(locale: string, driver: WebDriver, target: string) {
+        console.log('Footer::constructor()');
+        this.chromeDriver = driver;
+        this.logFilename = generateLogFileName(`footer-${locale}`);
+        this.totalTests = 4; // TODO: Calculate instead of hard code
+        this.targetURL = target;
+
+        this.startTime = 0;
+        this.passCounter = 0;
+        this.failCounter = 0;
+
+        switch (locale) {
+            case 'fr':
+                this.LABEL_ABOUT = FOOTER.FR_ABOUT;
+                this.LABEL_TERMS = FOOTER.FR_TERMS;
+                this.LABEL_PRIVACY = FOOTER.FR_PRIVACY;
+                this.LABEL_ACCESSIBILITY = FOOTER.FR_ACCESSIBILITY
+                this.ID_ABOUT = 'FOOTER-ANON-FR-002';
+                this.ID_TERMS = 'FOOTER-ANON-FR-003';
+                this.ID_PRIVACY = 'FOOTER-ANON-FR-004';
+                this.ID_ACCESSIBILITY = 'FOOTER-ANON-FR-005';
+                break;
+            case 'en':
+            default:
+                this.LABEL_ABOUT = FOOTER.EN_ABOUT;
+                this.LABEL_TERMS = FOOTER.EN_TERMS;
+                this.LABEL_PRIVACY = FOOTER.EN_PRIVACY;
+                this.LABEL_ACCESSIBILITY = FOOTER.EN_ACCESSIBILITY;
+                this.ID_ABOUT = 'FOOTER-ANON-EN-002';
+                this.ID_TERMS = 'FOOTER-ANON-EN-003';
+                this.ID_PRIVACY = 'FOOTER-ANON-EN-004';
+                this.ID_ACCESSIBILITY = 'FOOTER-ANON-EN-005';
+                break;
+        }
+    }// End of constructor()
+
+    private async runStep(cssSelector: string, stepCode: string) {
+        console.log(`${stepCode}->START`);
+        try {
+            const element = await this.chromeDriver.findElement(By.css(cssSelector));
+            await element.click();
+
+            const success_message = `${stepCode}->success\n`;
+            await appendFile(this.logFilename, success_message);
+            this.passCounter += 1;
+        } catch (error: any) {
+            const fail_message = `${stepCode}->onFailure ${error}\n`;
+            console.log(fail_message);
+            await appendFile(this.logFilename, fail_message);
+            this.failCounter += 1;
+        } finally {
+            // Reset page to original target
+            console.log(`${stepCode}->END`);
+            await this.chromeDriver.get(this.targetURL);
+            await this.chromeDriver.executeScript(
+                'window.scrollTo(0, document.body.scrollHeight);'
+            );
+        }
+    }// End of runStep()
+
+    // TODO: Handle when click opens a new tab - should go back to original tab
+    public async runTests() {
+        const startMessage = `Footer::runTests::targetURL->${this.targetURL}\n`
+        console.log(startMessage)
+        this.startTime = Date.now();
+        try {
+            await this.chromeDriver.executeScript(
+                'window.scrollTo(0, document.body.scrollHeight);'
+            );
+            await appendFile(this.logFilename, startMessage);
+
+            await this.chromeDriver.sleep(CLICK_DELAY);
+            await this.runStep(
+                this.LABEL_ABOUT,
+                this.ID_ABOUT
+            );
+
+            await this.chromeDriver.sleep(CLICK_DELAY);
+            await this.runStep(
+                this.LABEL_TERMS,
+                this.ID_TERMS
+            );
+
+            await this.chromeDriver.sleep(CLICK_DELAY);
+            await this.runStep(
+                this.LABEL_PRIVACY,
+                this.ID_PRIVACY
+            );
+
+            await this.chromeDriver.sleep(CLICK_DELAY);
+            await this.runStep(
+                this.LABEL_ACCESSIBILITY,
+                this.ID_ACCESSIBILITY
+            );
+
+            await this.chromeDriver.sleep(CLICK_DELAY);
+            await this.finish();
+        } catch (error: any) {
+            const fail_message = `Footer::runTests->onFailure\n${error}\n`;
+            console.log(fail_message)
+            await appendFile(this.logFilename, fail_message);
+        }
+    }// End of runTests()
+
+    private async finish() {
+        const endTime = Date.now();
+        const totalTime = endTime - this.startTime;
+
+        const summary = generateSummary(
+            this.totalTests,
+            this.passCounter,
+            this.failCounter,
+            totalTime
+        );
+
+        console.log('Footer::finish')
+        await appendFile(this.logFilename, summary);
+    }// End of finish()
+}
