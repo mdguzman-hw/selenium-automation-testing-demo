c01a2b0ed139b1ef330f9d3e6a3913b27a8d7421
Mark De Guzman <mdguzman@homewoodhealth.com>
2026-02-02 15:23:41 -0700

    Wire in Update PublicLanding Tests

--- src/tests/Smoke.ts
+++ src/tests/Smoke.ts
@@ -15,8 +15,7 @@ import { PublicLanding } from './public/PublicLanding';
  * Smoke - Test
  */
 async function Smoke() {
-    console.log('smoke->START\n');
-
+    console.log('Smoke->START\n');
     /*
      * 1: Set up
      */
@@ -26,16 +25,14 @@ async function Smoke() {
         '--incognito',
         // '--start-maximized',
     );
-
-    // 1.2 Variables
+    // 1.2: Variables
     const TARGET_URL_EN = HOMEWEB_LANDING_URL_EN;
     const TARGET_URL_FR = HOMEWEB_LANDING_URL_FR;
 
     // 2: Launch browser
     const chromeDriver = new Builder().forBrowser(Browser.CHROME).setChromeOptions(options).build();
-
     try {
-        // 2.1 Get browser window
+        // 2.1: Get browser window
         const windowHandle = await chromeDriver.getWindowHandle();
 
         /**
@@ -43,15 +40,12 @@ async function Smoke() {
          */
         // 3.1: Navigate to EN target
         await chromeDriver.get(TARGET_URL_EN)
-
         // 3.2: Header
-        // const header_en = new Header(LANGUAGE.ENGLISH, chromeDriver, TARGET_URL_EN, windowHandle);
-        // await header_en.runTests();
-
+        const header_en = new Header(LANGUAGE.ENGLISH, chromeDriver, TARGET_URL_EN, windowHandle);
+        await header_en.runTests();
         // 3.3: Footer
-        // const footer_en = new Footer(LANGUAGE.ENGLISH, chromeDriver, TARGET_URL_EN, windowHandle);
-        // await footer_en.runTests();
-
+        const footer_en = new Footer(LANGUAGE.ENGLISH, chromeDriver, TARGET_URL_EN, windowHandle);
+        await footer_en.runTests();
         // 3.4: Public - Landing
         const public_landing_en = new PublicLanding(LANGUAGE.ENGLISH, chromeDriver, TARGET_URL_EN, windowHandle);
         await public_landing_en.runTests();
@@ -61,15 +55,12 @@ async function Smoke() {
          */
         // 4.1: Navigate to FR target
         await chromeDriver.get(TARGET_URL_FR)
-
         // 4.2: Header
-        // const header_fr = new Header(LANGUAGE.FRENCH, chromeDriver, TARGET_URL_FR, windowHandle);
-        // await header_fr.runTests();
-
+        const header_fr = new Header(LANGUAGE.FRENCH, chromeDriver, TARGET_URL_FR, windowHandle);
+        await header_fr.runTests();
         // 4.3: Footer
-        // const footer_fr = new Footer(LANGUAGE.FRENCH, chromeDriver, TARGET_URL_FR, windowHandle);
-        // await footer_fr.runTests();
-
+        const footer_fr = new Footer(LANGUAGE.FRENCH, chromeDriver, TARGET_URL_FR, windowHandle);
+        await footer_fr.runTests();
         // 4.4: Public - Landing
         const public_landing_fr = new PublicLanding(LANGUAGE.FRENCH, chromeDriver, TARGET_URL_FR, windowHandle);
         await public_landing_fr.runTests();
@@ -77,21 +68,17 @@ async function Smoke() {
         /**
          * 5: Generate Report
          */
-        // const total = header_en.testTotal + header_fr.testTotal + footer_en.testTotal + footer_fr.testTotal + public_landing_en.testTotal + public_landing_fr.testTotal;
-        // const pass = header_en.passed + header_fr.passed + footer_en.passed + footer_fr.passed + public_landing_en.passed + public_landing_fr.passed;
-        // const fail = header_en.failed + header_fr.failed + footer_en.failed + footer_fr.failed + public_landing_en.failed + public_landing_fr.failed;
-        // console.log(generateReport(total, pass, fail));
-        console.log(generateReport(public_landing_en.testTotal, public_landing_en.passed, public_landing_en.failed));
-        console.log(generateReport(public_landing_fr.testTotal, public_landing_fr.passed, public_landing_fr.failed));
+        const total = header_en.testTotal + header_fr.testTotal + footer_en.testTotal + footer_fr.testTotal + public_landing_en.testTotal + public_landing_fr.testTotal;
+        const pass = header_en.passed + header_fr.passed + footer_en.passed + footer_fr.passed + public_landing_en.passed + public_landing_fr.passed;
+        const fail = header_en.failed + header_fr.failed + footer_en.failed + footer_fr.failed + public_landing_en.failed + public_landing_fr.failed;
+        console.log(generateReport(total, pass, fail));
     }
     catch (error: any) {
-        console.log('smoke->onFailure\n', error);
+        console.log('Smoke->onFailure\n', error);
     }
     finally {
-        console.log('smoke->END');
+        console.log('Smoke->END');
     }
     await chromeDriver.quit()
 }
-
 Smoke()
-
