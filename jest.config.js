295d23e4a47122a2d4f56f09717722c9758addf1
Mark De Guzman <mdguzman@homewoodhealth.com>
2026-02-03 13:08:47 -0700

    Add babel typescript jest dependencies and configurations

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node'
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.[jt]sx?$': 'babel-jest', // use Babel for JS/TS files
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    transformIgnorePatterns: [
+        'node_modules/(?!(make-plural|i18n-js)/)', // transform these ESM modules
+    ],
 };
